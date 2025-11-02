a5bb8244e0ff42f7e6eef19c4f533b21e3fb9d0c
omardeveloping <omar.montanares@inacapmail.cl>
2025-11-02 14:12:36 -0300

    + mirror volume

--- apismartlend/operaciones/models.py
+++ apismartlend/operaciones/models.py
@@ -22,3 +22,7 @@ class prestamo(models.Model):
     id_usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     id_herramienta_individual = models.ForeignKey('inventario.herramienta_individual', on_delete=models.CASCADE)
     id_tipo_herramienta = models.ForeignKey('inventario.tipo_herramienta', on_delete=models.CASCADE)
+
+class test(models.Model):
+    id_test = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50 )
