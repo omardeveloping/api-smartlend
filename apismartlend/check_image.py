34ff8871679b44689d81b9f501d54bc35091e7b8
omardeveloping <omar.montanares@inacapmail.cl>
2025-11-29 11:53:06 -0300

    +-dockerfile

--- apismartlend/check_image.py
+++
@@ -1,6 +0,0 @@
-import face_recognition
-
-path = r"C:\Users\omarc\OneDrive\Imágenes\Neme_cropped_rgb.jpg"
-image = face_recognition.load_image_file(path)
-print("shape:", image.shape, "dtype:", image.dtype)
-print("face_locations:", face_recognition.face_locations(image))
