0c7efb725e52990278e24f672a4aa7bd9a2302d4
omardeveloping <omar.montanares@inacapmail.cl>
2025-12-01 12:56:22 -0300

    +entrypoint

--- apismartlend/entrypoint.sh
+++ apismartlend/entrypoint.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
+# Wait for database DNS/port before running migrations
+echo "Waiting for database ${DATABASE_HOST:-db}:${DATABASE_PORT:-5432}..."
+until python - <<'PYCODE'
+import os, socket, sys
+host = os.environ.get("DATABASE_HOST", "db")
+port = int(os.environ.get("DATABASE_PORT", "5432"))
+s = socket.socket()
+s.settimeout(2)
+try:
+    s.connect((host, port))
+except Exception:
+    sys.exit(1)
+finally:
+    s.close()
+PYCODE
+do
+  echo "Database not ready, sleeping..."
+  sleep 2
+done
+
 # Aplica migraciones pendientes
 python manage.py migrate --noinput
 
