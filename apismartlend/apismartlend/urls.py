6918a2499a58a2ef387d65f0321c4eabc13eae43
Juan <juan.erices04@inacapmail.cl>
2026-06-29 20:12:55 -0400

    fix(backend): servir archivos media en produccion para docker/cloudflare

--- apismartlend/apismartlend/urls.py
+++ apismartlend/apismartlend/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +27,9 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Servir archivos media en desarrollo y producción (necesario en Docker/Cloudflare sin Nginx)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
